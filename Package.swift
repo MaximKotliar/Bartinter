602dee58d47466671a04edab353a2ca46ff505a0
Vladislav Prusakov <spectraldragonchannel@gmail.com>
2019-10-06 02:57:45 +0300

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -21,9 +21,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Bartinter",
-            dependencies: []),
-        .testTarget(
-            name: "BartinterTests",
-            dependencies: ["Bartinter"]),
+            dependencies: [])
     ]
 )
